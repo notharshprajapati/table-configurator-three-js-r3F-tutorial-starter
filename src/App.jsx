35b9eb1274016cef761be8b8eca7ae1589c4c609
notharshprajapati <notharsh0243@gmail.com>
2024-07-16 18:07:46 +0530

    presentation

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,12 @@ import { Experience } from "./components/Experience";
 function App() {
   return (
     <>
-      <Canvas shadows camera={{ position: [4, 4, -12], fov: 35 }}>
+      <Canvas shadows camera={{ position: [1, 1, 0], fov: 50 }}>
         <Experience />
+        <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.85, 0]}>
+          <planeGeometry args={[800, 800]} />
+          <meshStandardMaterial color={"black"} />
+        </mesh>
       </Canvas>
     </>
   );
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,28 +1,17 @@
 import { OrbitControls, Stage } from "@react-three/drei";
 import { Untitled } from "./Untitled";
-// import { Donut } from "./Donut";
-// import { Sphere } from "./Sphere";
+import { OneObst } from "./OneObst";
 export const Experience = () => {
   return (
     <>
-      <Stage
-        intensity={1.5}
-        shadows={{
-          type: "accumulative",
-          color: "#fff",
-          colorBlend: 2,
-          opacity: 2,
-        }}
-        adjustCamera={2}
-      >
-        <Untitled />
-        {/* <Donut /> */}
-        {/* <Sphere /> */}
+      <Stage adjustCamera={1}>
+        {/* <Untitled /> */}
+        <OneObst rotation={[0, Math.PI / 2, 0]} />
       </Stage>
       <OrbitControls
         makeDefault
         minPolarAngle={0}
-        maxPolarAngle={Math.PI / 2}
+        maxPolarAngle={Math.PI / 3}
       />
     </>
   );
